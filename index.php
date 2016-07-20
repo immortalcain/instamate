ca1ce445ad73576e1901d193fdaca339902f58a2
Kaan <kaan@mate.com.tr>
2016-07-21 02:03:32 +0300

    rev

--- index.php
+++ index.php
@@ -4,11 +4,7 @@ error_reporting(0);
 ?>
 <head>
 	<script src="instashow/jquery.instashow.packaged.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/handlebars.js/3.0.3/handlebars.runtime.min.js"></script>
 
-	<link href="instashow/jquery.instashow.min.css" rel="stylesheet">
-	<script src="instashow/jquery.instashow.min.js"></script>
 </head>
 
 
@@ -18,44 +14,15 @@ error_reporting(0);
      data-is-api="instashow/api/"
      data-is-source="#aklimdangecen"
      data-is-width="auto"
-     data-is-height="500px"
+     data-is-height="auto"
      data-is-columns="5"
      data-is-rows="3"
-     data-is-direction="vertical"
-     data-is-lang="en"
-     data-is-popup-info="">
+     data-is-direction="horizontal"
+     data-is-lang="tr">
 	</div>
 
 	<div class="my-instashow"></div>
 
-<script>
-    $('.my-instashow').instaShow({
-        api: 'instashow/api/',
-        source: '#aklimdangecen',
-        width: 'auto',
-        height: '500px',
-        columns: 5,
-        rows: 3,
-        direction: 'vertical',
-        lang: 'en',
-        popupInfo: ''
-    });
-</script>
-<script>
-    $(window).on('instaShowReady', function() {
-        $('.my-instashow').instaShow({
-            api: 'instashow/api/',
-            source: '#aklimdangecen',
-            width: 'auto',
-            height: '500px',
-            columns: 5,
-            rows: 3,
-            direction: 'vertical',
-            lang: 'en',
-            popupInfo: ''
-        });
-    });
-</script>
 
 </body>
 </html>
